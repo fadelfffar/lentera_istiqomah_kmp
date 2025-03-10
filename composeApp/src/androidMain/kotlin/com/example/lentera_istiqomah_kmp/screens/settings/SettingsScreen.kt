9ab0fb367e611201329d22c29cbbe974a969bce3
fadelfffar <fadel.farinsqi1739@gmail.com>
2025-03-10 10:53:19 +0700

    Implements BottomNavigation from https://www.geeksforgeeks.org/bottom-navigation-bar-in-android-jetpack-compose/
    and readjust Separation of Concerns

--- composeApp/src/androidMain/kotlin/com/example/lentera_istiqomah_kmp/screens/settings/SettingsScreen.kt
+++ composeApp/src/androidMain/kotlin/com/example/lentera_istiqomah_kmp/screens/settings/SettingsScreen.kt
@@ -4,24 +4,12 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Icon
-import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.outlined.DateRange
-import androidx.compose.material.icons.outlined.Home
 import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.unit.dp
-import cafe.adriel.voyager.core.screen.Screen
 
 
 @Composable
